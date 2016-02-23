82d497e12129e5e8d4d4afb2a17480bcf0493a90
Grzegorz <gb87@wp.pl>
2016-02-23 17:24:56 +0100

    Add validations to modal form with jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,17 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  $('#myModal').modal()
+  $('input[type=checkbox]').change(function(e){
+   if ($('input[type=checkbox]:checked').length > 3) {
+        $(this).prop('checked', false)
+   }
+  });
+  $("form").submit(function(){
+    if($(this).find("input:checked").length < 3){
+      alert("Hey boss. He is not here to rest. He needs to work hard now. Choose at least three skills for him to work on. We don't respect laziness.");
+      return false;
+    }
+  });
+});
