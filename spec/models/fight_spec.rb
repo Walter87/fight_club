d18aa7f2b0417eabe91e59f2a225c6ab436fd483
Grzegorz <gb87@wp.pl>
2016-02-27 18:20:23 +0100

    Writ some basic unit tests for fight model

--- spec/models/fight_spec.rb
+++ spec/models/fight_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Fight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do
+    it { is_expected.to belong_to(:fighter) }
+    it { is_expected.to belong_to(:opponent) }
+    it { is_expected.to have_one(:verdict) }
+  end
+  context "validations" do
+    it { is_expected.to validate_presence_of(:fighter_id) }
+    it { is_expected.to validate_presence_of(:opponent_id) }
+  end
 end
